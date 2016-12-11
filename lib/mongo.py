b90a0c3c2d6acd4344eac788cc294efc3696d746
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-12-10 21:50:09 -0800

    Resolved Merge Conflicts

--- lib/mongo.py
+++ lib/mongo.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
+from pymongo.operations import UpdateOne
 from external_services import DATABASE, MONGO_STRING
 
 client = MongoClient(MONGO_STRING + DATABASE)
 database = client[DATABASE]
+
 dockets = database['dockets']
+comments = database['comments']
 
 # def insertDockets (newDockets) :
 # 	dockets.insert(newDockets)
@@ -24,3 +27,16 @@ def retreiveDocket (docketID):
 	docket = dockets.find_one({'id': docketID})
 	return Docket
 
+def update_dockets(docket_items):
+    dockets.bulk_write(
+        [UpdateOne({'_id': item['_id']}, item) for item in docket_items])
+
+def retrieve_comments():
+    return comments.find()
+
+def retrieve_comments_by_docket_id(docket_id):
+    return comments.find({'docketId': docket_id})
+
+def update_comments(comment_items):
+    comments.bulk_write(
+        [UpdateOne({'_id': item['_id']}, item) for item in comment_items])
